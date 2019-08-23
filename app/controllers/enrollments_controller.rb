1ca85fffb7033653bd544dccbc443491e487ddbc
Kevin Northrup <knorthrup83@gmail.com>
2019-08-23 20:54:38 +0000

    Lesson 29, @Adding support for prem & free

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,7 +3,25 @@ class EnrollmentsController < ApplicationController
 
 	def create
 		current_user.enrollments.create(course: current_course)
-		redirect_to course_path(current_course)
+
+	    # Amount in cents
+	    @amount = (current_course.cost * 100).to_i
+
+	    customer = Stripe::Customer.create(
+	      email: params[:stripeEmail],
+	      source: params[:stripeToken]
+	    )
+
+	    charge = Stripe::Charge.create(
+	      customer: customer.id,
+	      amount: @amount,
+	      description: 'Flixter Premium Content',
+	      currency: 'usd'
+	    )
+	redirect_to course_path(current_course)
+	rescue Stripe::CardError => e
+		flash[:error] = e.message
+		redirect_to root_path
 	end
 
 	private
