db9f00ddf925ea3c11b059ea10c4fb984ed92c6d
Kevin Northrup <knorthrup83@gmail.com>
2019-08-24 19:32:12 +0000

    Finished lesson 29

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,7 @@ class EnrollmentsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		current_user.enrollments.create(course: current_course)
-
+		if current_course.premium?
 	    # Amount in cents
 	    @amount = (current_course.cost * 100).to_i
 
@@ -18,6 +17,8 @@ class EnrollmentsController < ApplicationController
 	      description: 'Flixter Premium Content',
 	      currency: 'usd'
 	    )
+	  end
+	current_user.enrollments.create(course: current_course)
 	redirect_to course_path(current_course)
 	rescue Stripe::CardError => e
 		flash[:error] = e.message
